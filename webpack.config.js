0b527dfde301c6d120429c33b1cbdf18290f05de
Ridho Maulana <edoin21@gmail.com>
2020-02-12 03:47:03 +0700

    Use webapck to compile to js

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  entry: "./src/index.ts",
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    modules: ["node_modules"]
+  },
+  externals: [nodeExternals()],
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
